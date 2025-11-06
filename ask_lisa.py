300c346389a963fb4846d5d1db66361986c7793e
Jannis Gollwitzer <info@jannisgollwitzer.de>
2025-11-06 10:10:23 +0000

    added lisa integration and system prompt

--- ask_lisa.py
+++ ask_lisa.py
@@ -7,26 +7,32 @@ with open("api_token.txt", encoding="UTF-8") as rf:
     api_token = rf.read().strip()
 
 
-def ask_lisa(question: str) -> str:
+def ask_lisa(system_prompt: str, question: str) -> str:
     global url
     global api_token
 
     model: str = "qwen/qwen3-next-80b"
     headers = {'Authorization': f'Bearer {api_token}',
-                "Content-Type": "application/json"}
+               "Content-Type": "application/json"}
     data = {
         "model": model,
         "messages": [
-        {
-          "role": "user",
-          "content": question
-        }
-      ]
+            {
+                "role": "system",
+                "content": system_prompt
+            },
+            {
+                "role": "user",
+                "content": question
+            }
+        ]
     }
     response = requests.post(url, json=data, headers=headers)
 
     print("Response status code:", response.status_code)
 
-    data = response.json()["choices"][0]["message"]["content"]
+    response_json = response.json()
+
+    data = response_json["choices"][0]["message"]["content"]
 
     return str(data)
