cd61cbd6548b4e0e2ba778272ebfa6aefb4d8404
Erik Rausch <erik.rausch@outlook.de>
2025-11-21 20:03:27 +0000

    added evaluation setup

--- training/evalation.py
+++ training/evalation.py
@@ -1,6 +1,14 @@
 from ask_lisa import ask_lisa
+import json
 
-with open("../../system_prompts/eval_answer_prompt.txt", "r") as f:
+eval_prompt_path = "../../system_prompts/eval_answer_prompt.txt"
+test_partition = "dataset/test.jsonl"
+
+out = "evaluation_results/"
+out_file = f"{out}test-results.jsonl"
+out_compact_file = f"{out}test-results.compact.json"
+
+with open(eval_prompt_path, "r") as f:
     eval_prompt = f.read()
 
 
@@ -16,3 +24,36 @@ def evaluate(context: str, question: str, expected_answer: str, generated_answer
     return int(res)
 
 
+dataset_entries = []
+
+with open(test_partition, encoding="UTF-8") as rf:
+    for line in rf:
+        entry = json.loads(line.strip())
+        dataset_entries.append(entry)
+
+results = {
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0
+}
+
+for entry in dataset_entries:
+    eval_value = evaluate(entry["context"], entry["question"], entry["answer"], entry["generated_answer"])
+    if eval_value == -1:
+        raise ValueError("The Lisa result had the wrong format.")
+
+    entry["eval"] = eval_value
+    results[eval_value] += 1
+
+with open(out_file, "w") as f:
+    json.dump(dataset_entries, f)
+
+with open(out_compact_file, "w") as f:
+    json.dump(results, f)
+
+print("Evaluation results: ")
+print(results)
+entry_count = results[1] + results[2] + results[3] + results[4]
+print(f"Accuracy: {results[1] / entry_count}")
+print(f"Real answers: {1 - results[4] / entry_count}")
