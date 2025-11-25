781c9626e6a22e9954aa5641a1a729d667453209
Jannis Gollwitzer <info@jannisgollwitzer.de>
2025-11-25 13:55:56 +0000

    added .gitignore and updated parquet-to-jsonl.py for audio file extraction

--- training/parquet-to-jsonl.py
+++ training/parquet-to-jsonl.py
@@ -1,10 +1,12 @@
+from pathlib import Path
+
 import pandas as pd
 import json
 
 wavs = "dataset/wavs/"
 
-input_file = "dataset-creation/out/validate.parquet"
-output_file = "dataset/output.jsonl"
+input_file = "../dataset-creation/out/validate.parquet"
+output_file = "dataset/output1.jsonl"
 
 
 def extract_parquet(parquet_file):
@@ -12,23 +14,66 @@ def extract_parquet(parquet_file):
 
 
 
-def parquet_to_jsonl(parquet_file, jsonl_file, exclude_columns=None):
+def parquet_to_jsonl(parquet_file, jsonl_file):
     df = pd.read_parquet(parquet_file)
 
-    # exclude binary columns
-    if exclude_columns:
-        existing_excludes = set(exclude_columns) & set(df.columns)
-        non_existing = set(exclude_columns) - set(df.columns)
-
-        if non_existing:
-            print(f"Spalten existieren nicht: {non_existing}")
-
-        df = df.drop(columns=list(existing_excludes))
+    # create dir if not existing
+    output_file_path = Path(jsonl_file)
+    output_file_path.parent.mkdir(parents=True, exist_ok=True)
 
     with open(jsonl_file, 'w', encoding='utf-8') as f:
         for _, row in df.iterrows():
-            json.dump(row.to_dict(), f, ensure_ascii=False)
+            base_path = f"{wavs}/{row.get('transcript_id')}"
+            target_dir = Path(base_path)
+            target_dir.mkdir(parents=True, exist_ok=True)
+
+            # context audio
+            with open(f"{base_path}/context.wav", 'wb') as af:
+                af.write(row.get("context_audio"))
+
+            # q1
+            json.dump({
+                'context_id': row.get("transcript_id"),
+                'context_text': row.get("context_text"),
+                'context_audio': f"{base_path}/context.wav",
+                'question_text': row.get("q1_text"),
+                'question_audio': f"{base_path}/q1.wav",
+                'answer_text': row.get("a1_text"),
+                'level': 1
+            }, f, ensure_ascii=False)
+
+            with open(f"{base_path}/q1.wav", 'wb') as af:
+                af.write(row.get("q1_audio"))
+
+            # q2
+            json.dump({
+                'context_id': row.get("transcript_id"),
+                'context_text': row.get("context_text"),
+                'context_audio': f"{base_path}/context.wav",
+                'question_text': row.get("q2_text"),
+                'question_audio': f"{base_path}/q2.wav",
+                'answer_text': row.get("a2_text"),
+                'level': 2
+            }, f, ensure_ascii=False)
+
+            with open(f"{base_path}/q2.wav", 'wb') as af:
+                af.write(row.get("q2_audio"))
+
+            # q3
+            json.dump({
+                'context_id': row.get("transcript_id"),
+                'context_text': row.get("context_text"),
+                'context_audio': f"{base_path}/context.wav",
+                'question_text': row.get("q3_text"),
+                'question_audio': f"{base_path}/q3.wav",
+                'answer_text': row.get("a3_text"),
+                'level': 3
+            }, f, ensure_ascii=False)
+
+            with open(f"{base_path}/q3.wav", 'wb') as af:
+                af.write(row.get("q3_audio"))
+
             f.write('\n')
 
 
-parquet_to_jsonl(input_file, output_file, ["context_audio", "q1_audio", "q2_audio", "q3_audio"])
+parquet_to_jsonl(input_file, output_file)
